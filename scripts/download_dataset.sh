4253c29b4d6fc79fafdebc24d1a53d12ddbff5bb
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2018-10-22 09:59:15 +0100

    Fix path to save dataset

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O ../scripts/slam_easy.bag http://dantecloud.isr.tecnico.ulisboa.pt/index.php/s/ooAKYGEoZAR47NR/download
+wget -O `rospack find turtlebot3_datasets`/data/slam_easy.bag http://dantecloud.isr.tecnico.ulisboa.pt/index.php/s/ooAKYGEoZAR47NR/download
